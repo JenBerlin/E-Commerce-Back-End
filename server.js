b2d4093528284c2a605e1f2af07599dfdd8f7c5b
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-12-13 18:31:44 +0100

    setting up files .env, .gitgnore, npm packages and sync database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const Connection = require("./config/connection");
+const routes = require("./routes");
 // import sequelize connection
 
 const app = express();
@@ -11,6 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+
+const init = async () => {
+  try {
+    await Connection.sync();
+    console.log(`[INFO]: DB connection successful`);
+
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  } catch (error) {
+    console.error(`[ERROR]: DB connection failed | ${error.message}`);
+  }
+};
+
+init();
